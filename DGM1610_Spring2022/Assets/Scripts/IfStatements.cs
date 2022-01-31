709859c2940b10a5ffefe2d16267444ab052e98c
Jack <flapjack.mail@gmail.com>
2022-01-30 19:17:38 -0700

    Variables and If Statements

---
+++ DGM1610_Spring2022/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    //public string lightColor;
+
+    //public string weather;
+
+    public string hairColor;
+
+    public int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(lightColor == "Red")
+         {
+             Debug.Log("The light is " + lightColor + ", STOP!!!!")
+         }
+         else if(lightColor == "Yellow")
+         {
+             Debug.Log("The light is " + lightColor + ", hold your horses there!")
+         }
+         else if (lightColor == "Yellow")
+         {
+             Debug.Log("The light is " + lightColor + ", GO GO GO!!!!")
+         }
+         else
+         {
+             Debug.Log("COLOR ERROR")
+         }
+
+         if(weather == "Rain" || weather == "Snow")
+         {
+             Debug.Log("the weather is shoddy, wear a fetching coat!");
+         }
+         else if(weather == "Sunny" || weather = = "Partly Cloudy")
+         {
+             Debug.Log("No coat needed, thanks!");
+         }
+        */
+        if (hairColor == "Red" && age == 6)
+        {
+            Debug.Log("You can't ride this ride, Little Mister!");
+        }
+        else if (hairColor == "Blonde" && age == 100)
+        {
+            Debug.Log("Sit down and enjoy the ride, sire.");
+        }
+
+
+    }
+}
